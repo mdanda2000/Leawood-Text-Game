e0f8f09f130f47c35ab1b7a738fa4773202aa91f
dandam <mdanda@yahoo.com>
2020-01-06 15:27:24 -0600

    Add files via upload
    Improved use of arrays

--- Matts Text Game/src/mattsTextGame/TextGame.java
+++ Matts Text Game/src/mattsTextGame/TextGame.java
@@ -28,9 +28,9 @@ public class TextGame {
 	boolean keepPlaying;	
 	
 	String[][] map = new String[99][6];
-	String[][] keyMap = new String[99][3];
+	int[][] keyMap = new int[99][3];
 	
-	ArrayList<String> keysFound = new ArrayList<String>();  //Stores the keys, which identify the nodes the user can pass through
+	ArrayList<Integer> keysFound = new ArrayList<Integer>();  //Stores the keys, which identify the nodes the user can pass through
 		
 	final int northNode = 0;       //The value in map[userLocation][northNode] identifies the node to the north
 	final int southNode = 1;       //The value in map[userLocation][southNode] identifies the node to the south
@@ -57,13 +57,14 @@ public class TextGame {
 	public void playerSetUp(){
 		
 		userLocation = 1;    // Set the player's initial location
-		keysFound.add("02");  //Give the player the key to the front door of the house
+		keysFound.add(2);  //Give the player the key to the front door of the house
 		
 		monsterLocation = 9; //Set the monster's initial location
 		
 		keepPlaying = true; 
 		
 		System.out.println("Welcome to my text game.");
+		printCommands();
 		System.out.println("Press ENTER to begin.");
 		myScanner.nextLine();
 		
@@ -76,17 +77,23 @@ public class TextGame {
 			File mapFile = new File ("C:\\Users\\Matt\\eclipse-workspace\\Matts Text Game\\src\\mattsTextGame\\LeawoodHouseMap.txt");
 			BufferedReader b = new BufferedReader(new FileReader(mapFile));
 			String readline = "";
-			int n=6; //number of parameters in each row, there's probably a way to use tokenizer to get it from readline 
+			
+			//int n=6; //number of parameters in each row, there's probably a way to use tokenizer to get it from readline 
+			
+			
 			int nodeRow=0; //node number, also the row number
 									
 			while ((readline=b.readLine())!= null){
 				//System.out.println(readline);  //For debugging purposes
 				StringTokenizer tokenizer = new StringTokenizer(readline, ",");				
 				
-				for (int i=0;i<n;i++){					
+				int n=tokenizer.countTokens();
+				System.out.println("int n=tokenizer.countTokens();  =  " + n);
+				
+				for (int i=0; i<n ;i++){					
 					String token = tokenizer.nextToken();					
 					map[nodeRow][i] = token;  //Populate array
-					//System.out.println("(" + nodeRow + "," + i + ") = " + map[nodeRow][i]); //For debugging purposes
+					System.out.println("(" + nodeRow + "," + i + ") = " + map[nodeRow][i]); //For debugging purposes
 				}
 				nodeRow++;						
 			}		
@@ -116,7 +123,7 @@ public class TextGame {
 				
 				for (int i=0;i<n;i++){					
 					String token = tokenizer.nextToken();					
-					keyMap[nodeRow][i] = token;  //Populate array					
+					keyMap[nodeRow][i] = Integer.parseInt(token);  //Populate array					
 				}
 				System.out.println("Key for node " + keyMap[nodeRow][0] + " is in node " + keyMap[nodeRow][1]); //For debugging purposes
 				nodeRow++;						
@@ -132,6 +139,18 @@ public class TextGame {
 	public void printStatus(){		
 		System.out.println(map[userLocation][descriptionNode] + " (" + userLocation + ", " + map[userLocation][4] +")");
 		System.out.println("The dog is in the " + map[monsterLocation][nameNode] + " (" + monsterLocation + ")");
+	}
+	
+	public void printCommands() {
+		System.out.println();
+		System.out.println("Valid commands are:");
+		System.out.println("  n = North");
+		System.out.println("  s = South");
+		System.out.println("  e = East");
+		System.out.println("  w = West");
+		System.out.println("  l = Look Around");
+		System.out.println("  q = Quit Game");
+		System.out.println();
 	}
 	
 	public void playGame(){
@@ -206,24 +225,24 @@ public class TextGame {
 				keepPlaying=false;
 				break;
 			default:
-				System.out.println("Valid commands are: n,s,e,w,l,q");
+				printCommands();
+				//System.out.println("Valid commands are: n,s,e,w,l,q");
 			}	
 			
-			// Check if the new location has any keys -- possibly move to new method
+			checkForKey(userLocation); // Check if the new node location has any keys
 			
-			// TO DO: If the node contains a key, verify that the key does not already exist in keysFound list
-			// TO DO: Add key to keysFound, notify player
+			
 			
 		}
 		
 		// This code runs after keepPlaying is set to false
-		System.out.println("--Quit!--");		
+		System.out.println("--You quit!--");		
 	}	
 	
 	public boolean isDoorUnlocked(int nodeNumber) {
 		if (map[nodeNumber][nameNode].equals("doorway")){
-			for (String i : keysFound) {
-				if (nodeNumber == Integer.parseInt(i)) {
+			for (int i : keysFound) {
+				if (nodeNumber == i) {
 					System.out.println("The door is locked, but you have a key!");
 					return true;  // User has the key
 				}
@@ -232,5 +251,32 @@ public class TextGame {
 			return false; // The door is locked and the above loop did not find the key in the list
 		}
 		return true;  // The node isn't a door, so it's unlocked by default
+	}
+	
+	public void checkForKey(int nodeNumber) {
+		// Check whether the node contains a key. Compare the nodeNumber with the values in keyMap[i][1]
+		
+	
+		//String nodeNumberString = Integer.toString(nodeNumber); 
+		
+		for (int i=0; i < keyMap.length; i++) {  //Check every row in the keyMap[][] array
+			if (nodeNumber == keyMap[i][1]) {
+				if (keyMap[i][2]==1) {
+					//Add the key to the keysFound array
+					keysFound.add(keyMap[i][0]);   
+					
+					//Set the value in keyMap[i][2] to "0" to indicate that the key is no longer in the node
+					keyMap[i][2] = 0;
+					
+					System.out.println("You found a key! " + keyMap[i][0]);
+				}
+								
+			}
+		}
+		
+		
+		//verify that the key does not already exist in keysFound list
+		// TO DO: Add key to keysFound, notify player
+		
 	}
 }
